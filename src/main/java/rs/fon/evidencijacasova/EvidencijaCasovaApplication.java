461731e163f9af659e0298faf18c72305983033e
boskovicdd <boskovic.z.david@gmail.com>
2026-07-03 14:42:04 +0200

    Inicijalni Spring Boot skelet

---
+++ src/main/java/rs/fon/evidencijacasova/EvidencijaCasovaApplication.java
@@ -0,0 +1,13 @@
+package rs.fon.evidencijacasova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidencijaCasovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvidencijaCasovaApplication.class, args);
+    }
+
+}
